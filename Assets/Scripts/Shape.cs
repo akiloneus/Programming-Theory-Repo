44d343c9ab42498c4f4edc3f03e91a8a8e8903a9
Akiloneus <akiloneus@outlook.com>
2023-07-12 00:20:44 +0800

    Implemented the function for title screen

---
+++ Assets/Scripts/Shape.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Shape : MonoBehaviour
+{
+    [SerializeField] protected TMP_Text text;
+    [SerializeField] protected new string name;
+
+    protected GameObject mainManager;
+
+    private void Start()
+    {
+        mainManager = GameObject.Find("MainManager");
+    }
+
+    // Encapculation
+    public string _name
+    {
+        get { return name; }
+        private set { name = value; }
+    }
+
+    public string _username { get; set; } 
+    public Color color { get; set; }
+
+    public virtual void Greet()
+    {
+        SetTextColor();
+        _username = mainManager.GetComponent<MainManager>().username;
+        text.text = $"{_name}: Hello, {_username}. Glad to meet you.";
+    }
+
+    // Abstraction
+    public void SetTextColor()
+    {
+        color = this.GetComponent<MeshRenderer>().material.color;
+        text.color = color;
+    }
+
+    public void OnMouseDown()
+    {
+        Greet();
+    }
+}
---
+++ Assets/Scripts/Sphere.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Sphere : Shape
+{
+    //Polymorphism
+    public override void Greet()
+    {
+        SetTextColor();
+        text.text = "Get off! You piece of *%@#! ";
+    }
+}
